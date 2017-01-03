ad10cfbcd14674a682f01c4138d4dc9b283451e3
Felipe Andrighetti <felipetirus@gmail.com>
2017-01-03 09:34:33 -0200

    Adding express structure and a static page

---
+++ config/express.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+module.exports = function() {
+  var app = express();
+
+  //Environment variables
+  app.set('port', 3000);
+
+  //middleware
+  app.use(express.static('./public'))
+  return app;
+}
