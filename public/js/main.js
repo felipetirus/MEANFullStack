08f779309196f62ecc169b89b3ce8ce86cef646c
Felipe Andrighetti <felipetirus@gmail.com>
2017-01-06 11:13:10 -0200

    Creating routes

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
-angular.module('contatooh', ['ngRoute']);
+angular.module('contatooh', ['ngRoute'])
+  .config(function($routeProvider) {
+    $routeProvider.when('/contatos', {
+      templateUrl: 'partials/contatos.html',
+      controller: 'ContatosController'
+    })
+  });
