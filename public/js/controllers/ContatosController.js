530db7cf76763215a026560d9636fa9f018ba530
Felipe Andrighetti <felipetirus@gmail.com>
2017-01-08 18:45:59 -0200

    Integrating front and back end

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,4 +1,6 @@
-angular.module('contatooh').controller('ContatosController', function($scope) {
+angular.module('contatooh').controller('ContatosController', function($scope, $http) {
+
+  $scope.contatos = [];
   $scope.total =0;
   $scope.filtro = '';
 
@@ -6,12 +8,13 @@ angular.module('contatooh').controller('ContatosController', function($scope) {
     $scope.total++;
   };
 
-  $scope.contatos = [
-    {_id: 1, nome: 'Contato Exemplo 1',
-     email: 'cont1@empresa.com.br'},
-    {_id: 2, nome: 'Contato Exemplo 2',
-     email: 'cont2@empresa.com.br'},
-    {_id: 3, nome: 'Contato Exemplo 3',
-     email: 'cont3@empresa.com.br'}
-  ];
+  $http.get('/contatos')
+  .success(function(data){
+    $scope.contatos = data;
+  })
+  .error(function(statusText){
+    console.log("Não foi possível obter a lista de contatos");
+    console.log(statusText);
+  });
+
 });
