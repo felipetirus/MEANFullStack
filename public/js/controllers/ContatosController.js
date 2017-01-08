1be999904d72899e1ac950b24f0491fb71ce8c42
Felipe Andrighetti <felipetirus@gmail.com>
2017-01-08 12:23:44 -0200

    Adding ng-repeat and a list of contacts

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -4,4 +4,13 @@ angular.module('contatooh').controller('ContatosController', function($scope) {
   $scope.incrementa = function() {
     $scope.total++;
   };
+
+  $scope.contatos = [
+    {_id: 1, nome: 'Contato Exemplo 1',
+     email: 'cont1@empresa.com.br'},
+    {_id: 2, nome: 'Contato Exemplo 2',
+     email: 'cont2@empresa.com.br'},
+    {_id: 3, nome: 'Contato Exemplo 3',
+     email: 'cont3@empresa.com.br'}
+  ];
 });
