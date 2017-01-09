1d156e5fcd091027dc218eec750ff7629aa3d1d9
Felipe Andrighetti <felipetirus@gmail.com>
2017-01-09 10:19:27 -0200

    Getting the contato data

--- public/js/controllers/ContatoController.js
+++ public/js/controllers/ContatoController.js
@@ -1,3 +1,19 @@
-angular.module('contatooh').controller('ContatoController', function($scope, $routeParams) {
-  console.log($routeParams.contatoId);
+angular.module('contatooh').controller('ContatoController', function($scope, $routeParams, $resource) {
+  var Contato = $resource('/contatos/:id');
+
+  function buscaContato() {
+    Contato.get({id: $routeParams.contatoId},
+      function(contato){
+        $scope.contato = contato;
+      },
+      function(erro){
+        console.log(erro);
+        $scope.mensagem= {
+          texto: 'Não foi possível obter o contato'
+        };
+      }
+    );
+  }
+
+  buscaContato();
 });
