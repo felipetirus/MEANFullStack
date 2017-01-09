d91d24ec64d1530bfec97682bb798efc86d8b057
Felipe Andrighetti <felipetirus@gmail.com>
2017-01-09 09:38:37 -0200

    Adding error message

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -3,6 +3,7 @@ angular.module('contatooh').controller('ContatosController', function($scope, $r
   $scope.contatos = [];
   $scope.total =0;
   $scope.filtro = '';
+  $scope.mensagem = {texto: ''};
 
   $scope.incrementa = function() {
     $scope.total++;
@@ -16,8 +17,10 @@ angular.module('contatooh').controller('ContatosController', function($scope, $r
         $scope.contatos = contatos;
       },
       function(erro){
-        console.log('Não foi possível obter a lista de contatos');
         console.log(erro);
+        $scope.mensagem= {
+          texto: 'Não foi possível obter a lista de contatos'
+        };
       }
     );
   }
@@ -28,8 +31,10 @@ angular.module('contatooh').controller('ContatosController', function($scope, $r
     var promise = Contato.delete({id : contato._id},
       buscaContatos,
       function(erro){
-        console.log('Não foi possível remover o contato');
         console.log(erro);
+        $scope.mensagem= {
+          texto: 'Não foi possível remover o contato'
+        };
       }
     );
   };
