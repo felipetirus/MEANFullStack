8524e3b2e0a1958c08639e8eae8f52645a408966
Felipe Andrighetti <felipetirus@gmail.com>
2017-01-11 15:38:41 -0200

    Adding connection to mongo

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,11 +1,4 @@
-var contatos = [
-  {_id: 1, nome: 'Contato Exemplo 1',
-   email: 'cont1@empresa.com.br'},
-  {_id: 2, nome: 'Contato Exemplo 2',
-   email: 'cont2@empresa.com.br'},
-  {_id: 3, nome: 'Contato Exemplo 3',
-   email: 'cont3@empresa.com.br'}
-];
+var contatos = [];
 
 var ID_CONTATO_INC = 3;
 
