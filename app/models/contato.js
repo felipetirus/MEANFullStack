8d40bfcaef672edbbcbb638b354da316f152ccdb
Felipe Andrighetti <felipetirus@gmail.com>
2017-01-12 17:21:38 -0200

    Adding authentication config part-1

--- app/models/contato.js
+++ app/models/contato.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var findOrCreate = require('mongoose-findorcreate');
 
 module.exports = function () {
   var schema = mongoose.Schema({
@@ -18,5 +19,6 @@ module.exports = function () {
       ref: 'Contato'
     }
   });
+  schema.plugin(findOrCreate);
   return mongoose.model('Contato', schema);
 };
